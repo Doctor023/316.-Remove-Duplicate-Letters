7ea1dbd8e5bb5c396b94f290af89b4e2cd088d47
Doctor023 <109444069+Doctor023@users.noreply.github.com>
2023-11-03 11:43:33 +0300

    Добавьте файлы проекта.

---
+++ 316. Remove Duplicate Letters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _316._Remove_Duplicate_Letters
+{
+    // Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is 
+    // the smallest in lexicographical order
+    // among all possible results.
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                List<char> doubles = new List<char>();
+                List<char> values = new List<char>();
+                string? symbols;
+                Console.WriteLine("Enter values");
+                symbols = Console.ReadLine();
+                if (symbols != null)
+                {
+                    foreach (char s in symbols)
+                    {
+                        switch (doubles.Contains(s))
+                        {
+                            case false:
+                                doubles.Add(s);
+                                values.Add(s);
+                                    break;
+                        }
+                    }
+                }   
+                var sortValues = values.OrderBy( w => w );
+                foreach (var sortValue in sortValues)
+                {
+                    Console.Write(sortValue);                  
+                }
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
